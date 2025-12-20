80f0f99c10808d20c44bc2306ca330f3af01f3b3
SANGH2021 <ssanghratna@gmail.com>
2025-12-20 21:01:44 +0530

    BOTTOM SHEET

--- lib/add_expense.dart
+++ lib/add_expense.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
 
 class AddExpense extends StatelessWidget {
-  const AddExpense({super.key});
+  AddExpense({super.key});
+
+  final List<Map<String, dynamic>> category = [
+    {
+      "categoryPic": "assets/travel.png",
+    },
+    {
+      "categoryPic": "assets/cup.png",
+    },
+    {
+      "categoryPic": "assets/popcorn.png",
+    },
+    {
+      "categoryPic": "assets/ambulance.png",
+    },
+    {
+      "categoryPic": "assets/mobile-transfer.png",
+    },
+    {
+      "categoryPic": "assets/laundry.png",
+    },
+    {
+      "categoryPic": "assets/life-insurance.png",
+    },
+    {
+      "categoryPic": "assets/restaurant.png"
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -40,10 +67,40 @@ class AddExpense extends StatelessWidget {
                 onChanged: (index) {},
               ),
               commonSizedBox(50),
+
               CommonElevatedButton(
                 "Choose Expense",
                 Colors.black,
                 Colors.white,
+                  (){
+                    showModalBottomSheet(
+
+                      context: context,
+                      builder: (context) {
+                        return Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: GridView.builder(
+
+                            itemCount: category.length,
+
+                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              mainAxisSpacing: 10,
+                              crossAxisSpacing: 10,
+                              crossAxisCount: 4,
+                              mainAxisExtent: 82,
+
+
+                            ),
+                            itemBuilder: (context, index){
+                              return Container(decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15), color: Color (0xFFD1E5EE),
+                              ),child: Center(child: SizedBox(height:60, width:60,child: IconButton(onPressed: (){}, icon: Image.asset(category[index]["categoryPic"])))),);
+                            },
+                          ),
+                        );
+                      },
+                    );
+                  },
               ),
               commonSizedBox(10),
 
@@ -51,9 +108,12 @@ class AddExpense extends StatelessWidget {
                 "December 14, 2023",
                 Colors.white,
                 Colors.black,
+                  (){
+                  showDatePicker(context: context, firstDate: DateTime(2000), lastDate: DateTime(2099));
+                  },
               ),
               commonSizedBox(10),
-              CommonElevatedButton("ADD Expense", Colors.black, Colors.white),
+              CommonElevatedButton("ADD Expense", Colors.black, Colors.white, (){}),
             ],
           ),
         ),
@@ -65,11 +125,12 @@ class AddExpense extends StatelessWidget {
     String childText,
     Color backgroundColor,
     Color foregroundColor,
+      onPressedd,
   ) {
     return SizedBox(
       width: double.infinity,
       child: ElevatedButton(
-        onPressed: () {},
+        onPressed: onPressedd,
         style: ElevatedButton.styleFrom(
           backgroundColor: backgroundColor,
           foregroundColor: foregroundColor,
